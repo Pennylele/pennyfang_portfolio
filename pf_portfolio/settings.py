482a480167259566e808c9aa4d9585c1892a8383
penny <fangle0121@gmail.com>
2020-12-28 19:40:43 -0800

    postgres db

--- pf_portfolio/settings.py
+++ pf_portfolio/settings.py
@@ -81,14 +81,24 @@ WSGI_APPLICATION = 'pf_portfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'pf_portfolio',
+            'USER': 'pf_porfolio',
+            'PASSWORD': 'sh!nyWing84', # Need to secure this.
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
