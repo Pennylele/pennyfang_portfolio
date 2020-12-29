f48de3c616cb5ad7cd739b2f8d49e8bcf6d5a4f1
penny <fangle0121@gmail.com>
2020-12-28 22:48:30 -0800

    changing debug mode

--- pf_portfolio/settings.py
+++ pf_portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('mock_secrete_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['167.71.161.121', '127.0.0.1', 'localhost']
 
