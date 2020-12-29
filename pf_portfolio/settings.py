de088a01cb55c14e2076d6f6988abc77dbef84fb
penny <fangle0121@gmail.com>
2020-12-28 18:09:51 -0800

    changing requirements.txt and settings.py

--- pf_portfolio/settings.py
+++ pf_portfolio/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*jo-qk*y$n9m7=&4!kqr49!znso@9kg8ao3!ue&6*)p_%&^jzd'
+SECRET_KEY = os.environ.get('mock_secrete_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
