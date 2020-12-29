02ca4f0cea37fc014bba014af7e4dfc27edf31a7
penny <fangle0121@gmail.com>
2020-12-29 00:39:36 -0800

    fix a typo

--- pf_portfolio/settings.py
+++ pf_portfolio/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('mock_secrete_key')
 SECRET_KEY = "646a01a540d54d7b9bbc821fb28ffd32c5b033200d8c88cc"
-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
